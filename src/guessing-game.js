f29b78377526f97baa877e23cec53df687239f4c
kirillabcd <k.motsart@gmail.com>
2023-10-15 21:01:03 +0300

    completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,37 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.max = max
+        this.min = min
 
+        this.array = []
+
+        for (let i = this.min; i <= this.max; i++) {
+            this.array.push(i)
+        }
     }
 
     guess() {
-
+        return this.array[Math.round((this.array.length - 1) / 2)]
     }
 
     lower() {
+        this.max = this.guess()
+        this.array = []
 
+        for (let i = this.min; i <= this.max; i++) {
+            this.array.push(i)
+        }
     }
 
     greater() {
+        this.min = this.guess()
+        this.array = []
 
+        for (let i = this.min; i <= this.max; i++) {
+            this.array.push(i)
+        }
     }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
